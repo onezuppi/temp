23a8a0a3f2f7792c1c3ae19caf264b175937e141
Sergey Dorokhov <seregapax@gmail.com>
2024-10-19 22:01:17 +0500

    install

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Загрузка переменных из .env
+if [ -f .env ]; then
+  export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
+else
+  echo ".env file not found!"
+  exit 1
+fi
+
 # Sequentially call the individual scripts
 bash scripts/install_deps.sh
 bash scripts/issue_certificate.sh
