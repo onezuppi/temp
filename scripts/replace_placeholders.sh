04bb762d3d15c96c1138c808b1830f98b6f1ce53
Sergey Dorokhov <seregapax@gmail.com>
2024-10-19 21:07:04 +0500

    fix

--- scripts/replace_placeholders.sh
+++ scripts/replace_placeholders.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-# Замена плейсхолдеров в файлах конфигурации
+# Функция для замены плейсхолдеров в конфигурации NGINX
 replace_placeholders() {
-    NGINX_CONF="nginx/nginx.conf"
-    ENV_FILE=".env"
+    NGINX_CONF_TEMPLATE="nginx/nginx-template.conf"  # Шаблонный файл
+    NGINX_CONF="nginx/nginx.conf"                   # Результирующий файл
+    ENV_FILE=".env"                                 # Файл с переменными
 
-    sed -i "s/my_domain\.com/$DOMAIN/g" "marzban/templates/singbox/default.json"
-    sed -i "s/panel\.my_domain\.com/${PANEL_SUBDOMAIN}.${DOMAIN}/g" "$NGINX_CONF"
-    sed -i "s/sub\.my_domain\.com/${SUB_SUBDOMAIN}.${DOMAIN}/g" "$NGINX_CONF"
-    sed -i "s|/sub/|$LOCATION_PATH|g" "$NGINX_CONF"
+    # Загружаем переменные из .env
+    if [ -f "$ENV_FILE" ]; then
+        export $(grep -v '^#' "$ENV_FILE" | xargs)
+    else
+        echo "Файл $ENV_FILE не найден!"
+        exit 1
+    fi
+
+    # Используем sed для замены только необходимых переменных
+    sed -e "s|\${DOMAIN}|$DOMAIN|g" \
+        -e "s|\${FAKE_SITE}|$FAKE_SITE|g" \
+        -e "s|\${PANEL_SUBDOMAIN}|$PANEL_SUBDOMAIN|g" \
+        -e "s|\${SUB_SUBDOMAIN}|$SUB_SUBDOMAIN|g" \
+        "$NGINX_CONF_TEMPLATE" > "$NGINX_CONF"
 
     echo "nginx.conf updated with domain-specific entries."
 }
 
+# Вызов функции
 replace_placeholders
